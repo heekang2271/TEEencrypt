3ee416541ccb583d657f4e7672acf72af365e2dd
heekang2271 <gmlrkd3922@gmail.com>
2021-05-10 20:38:32 +0900

    Caesar 구현

---
+++ host/main.c
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2016, Linaro Limited
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <err.h>
+#include <stdio.h>
+#include <string.h>
+
+/* OP-TEE TEE client API (built by optee_client) */
+#include <tee_client_api.h>
+
+/* To the the UUID (found the the TA's h-file(s)) */
+#include <TEEencrypt_ta.h>
+
+int main(int argc, char *argv[])
+{
+	TEEC_Result res;
+	TEEC_Context ctx;
+	TEEC_Session sess;
+	TEEC_Operation op;
+	TEEC_UUID uuid = TA_TEEencrypt_UUID;
+	uint32_t err_origin;
+
+	int len=64;
+	char plaintext[64] = {0,};
+	char ciphertext[64] = {0,};
+
+	res = TEEC_InitializeContext(NULL, &ctx);
+	if (res != TEEC_SUCCESS)
+		errx(1, "TEEC_InitializeContext failed with code 0x%x", res);
+
+	res = TEEC_OpenSession(&ctx, &sess, &uuid,
+			       TEEC_LOGIN_PUBLIC, NULL, NULL, &err_origin);
+	if (res != TEEC_SUCCESS)
+		errx(1, "TEEC_Opensession failed with code 0x%x origin 0x%x",
+			res, err_origin);
+
+	memset(&op, 0, sizeof(op));
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_OUTPUT, TEEC_VALUE_INOUT,
+					 TEEC_NONE, TEEC_NONE);
+	op.params[0].tmpref.buffer = plaintext;
+	op.params[0].tmpref.size = len;
+
+	if (argc == 0 || argc > 5) {
+		printf("Invalid parameter.\n");
+		TEEC_CloseSession(&sess);
+		TEEC_FinalizeContext(&ctx);
+		return 0;
+	}
+	
+	if (strcmp(argv[1], "-e") == 0) {
+		FILE* ptFile;
+		FILE* keyFile;
+		FILE* ctFile;
+		int key;
+		char keyBuf[10] = {0, };
+
+		ptFile = fopen(argv[2], "r");
+		fread(plaintext, sizeof(plaintext), 1, ptFile);
+		fclose(ptFile);
+		memcpy(op.params[0].tmpref.buffer, plaintext, len);
+
+		if (strcmp(argv[3], "Caesar") == 0) {
+			res = TEEC_InvokeCommand(&sess, TA_MYTA_CMD_ENC_VALUE, &op,
+					 &err_origin);
+			if (res != TEEC_SUCCESS)
+				errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x",
+					res, err_origin);
+			memcpy(ciphertext, op.params[0].tmpref.buffer, len);
+			ctFile = fopen("ciphertext.txt", "w");
+			fwrite(ciphertext, strlen(ciphertext), 1, ctFile);
+			fclose(ctFile);
+
+			key = op.params[1].value.a;
+			sprintf(keyBuf, "%d", key);
+			keyFile = fopen("cipher_key.txt", "w");
+			fwrite(keyBuf, strlen(keyBuf), 1, keyFile);
+			fclose(keyFile);
+
+		} else if (strcmp(argv[3], "RSA") == 0) {
+
+		}
+	} else if (strcmp(argv[1], "-d") == 0) {
+		FILE* ptFile;
+		FILE* keyFile;
+		FILE* ctFile;
+
+		ctFile = fopen(argv[2], "r");
+		fread(ciphertext, sizeof(ciphertext), 1, ctFile);
+		fclose(ctFile);
+		memcpy(op.params[0].tmpref.buffer, ciphertext, len);
+
+		if (strcmp(argv[4], "Caesar") == 0) {
+			int atoiKey;
+			char keyBuf[10] = {0, };
+
+			keyFile = fopen(argv[3], "r");
+			fread(keyBuf, sizeof(keyBuf), 1, keyFile);
+			fclose(keyFile);
+			atoiKey = atoi(keyBuf);
+			op.params[1].value.a = atoiKey;
+
+			res = TEEC_InvokeCommand(&sess, TA_MYTA_CMD_DEC_VALUE, &op,
+					 &err_origin);
+			if (res != TEEC_SUCCESS)
+				errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x",
+					res, err_origin);
+
+			memcpy(plaintext, op.params[0].tmpref.buffer, len);
+			
+			ptFile = fopen("dec_plaintext.txt", "w");
+			fwrite(plaintext, strlen(plaintext), 1, ptFile);
+			fclose(ptFile);
+		} else if (strcmp(argv[4], "RSA") == 0) {
+
+		}
+	}
+
+
+	TEEC_CloseSession(&sess);
+	TEEC_FinalizeContext(&ctx);
+
+	return 0;
+}
---
+++ ta/Android.mk
@@ -0,0 +1,4 @@
+LOCAL_PATH := $(call my-dir)
+
+local_module := a52a67f8-58f2-4e80-9bd9-526e7a375074.ta
+include $(BUILD_OPTEE_MK)
